ea05a71b52f6ec2122416263718ee97ea832b436
Aggrey Omondi <aggreyomondi7@gmail.com>
2024-12-12 16:03:48 +0300

    updates

--- projects/neuralangelo/scripts/preprocess.sh
+++ projects/neuralangelo/scripts/preprocess.sh
@@ -13,6 +13,6 @@
 data_path=datasets/${1}_ds${3}
 bash projects/neuralangelo/scripts/run_ffmpeg.sh ${1} ${2} ${3}
 bash projects/neuralangelo/scripts/debug.py
-#bash projects/neuralangelo/scripts/run_colmap.sh ${data_path}
-#python3 projects/neuralangelo/scripts/convert_data_to_json.py --data_dir ${data_path} --scene_type ${4}
-#python3 projects/neuralangelo/scripts/generate_config.py --sequence_name ${1} --data_dir ${data_path} --scene_type ${4}
+bash projects/neuralangelo/scripts/run_colmap.sh ${data_path}
+python3 projects/neuralangelo/scripts/convert_data_to_json.py --data_dir ${data_path} --scene_type ${4}
+python3 projects/neuralangelo/scripts/generate_config.py --sequence_name ${1} --data_dir ${data_path} --scene_type ${4}
