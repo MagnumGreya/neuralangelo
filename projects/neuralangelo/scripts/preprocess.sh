7b3b6f988b29e66d1bad987e35fcd863f8701624
Aggrey Omondi <aggreyomondi7@gmail.com>
2024-12-12 16:47:49 +0300

    updates

--- projects/neuralangelo/scripts/preprocess.sh
+++ projects/neuralangelo/scripts/preprocess.sh
@@ -14,7 +14,10 @@ data_path=datasets/${1}_ds${3}
 bash projects/neuralangelo/scripts/run_ffmpeg.sh ${1} ${2} ${3}
 
 git clone https://github.com/MagnumGreya/deep-image-matching.git
-bash projects/neuralangelo/scripts/debug.py
+cd ./deep-image-matching/
+python3 main.py --dir ../${data_path} --pipeline superpoint+lightglue
+
+python3 debug.py
 #bash projects/neuralangelo/scripts/run_colmap.sh ${data_path}
 #python3 projects/neuralangelo/scripts/convert_data_to_json.py --data_dir ${data_path} --scene_type ${4}
 #python3 projects/neuralangelo/scripts/generate_config.py --sequence_name ${1} --data_dir ${data_path} --scene_type ${4}
