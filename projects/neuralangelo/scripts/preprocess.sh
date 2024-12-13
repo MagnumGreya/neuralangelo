a22811f5682acd6d0a9c031eb7be82194a61772a
Aggrey Omondi <aggreyomondi7@gmail.com>
2024-12-13 14:01:23 +0300

    updates

--- projects/neuralangelo/scripts/preprocess.sh
+++ projects/neuralangelo/scripts/preprocess.sh
@@ -12,8 +12,11 @@
 
 data_path=datasets/${1}_ds${3}
 bash projects/neuralangelo/scripts/run_ffmpeg.sh ${1} ${2} ${3}
-cp ../data_colmap/results_aliked+lightglue_matching_lowres_quality_high/database.db ${data_path} #Modified depending on the data from DIM
-bash projects/neuralangelo/scripts/run_colmap.sh ${data_path}
-python3 projects/neuralangelo/scripts/convert_data_to_json.py --data_dir ${data_path} --scene_type ${4}
-python3 projects/neuralangelo/scripts/generate_config.py --sequence_name ${1} --data_dir ${data_path} --scene_type ${4}
+
+git clone https://github.com/MagnumGreya/deep-image-matching.git
+cd ./deep-image-matching/
+python3 main.py --data_dir ../${data_path} --pipeline superpoint+lightglue --verbose 
+#bash projects/neuralangelo/scripts/run_colmap.sh ${data_path}
+#python3 projects/neuralangelo/scripts/convert_data_to_json.py --data_dir ${data_path} --scene_type ${4}
+#python3 projects/neuralangelo/scripts/generate_config.py --sequence_name ${1} --data_dir ${data_path} --scene_type ${4}
 python3 projects/neuralangelo/scripts/debug.py
